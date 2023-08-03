816bf7e35bc5580557aa8a4d16a5d5c560826db5
Rodrigo Morais <rcastromorais01@gmail.com>
2023-08-03 17:06:31 -0300

    start with server config

---
+++ src/test/java/deliveryconfigserver/deliveryconfigserver/MicroservicesExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package deliveryconfigserver.deliveryconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
